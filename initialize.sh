b339f417c7f97daef70a062b8485cd42bf61ee87
adamsaj <adamsaj@me.com>
2015-05-30 15:45:40 -0500

    fixes more bugs in initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ##initializes new document from template
 ##usage: ./initialize.sh <document_name_without_extension>
+##by: Andrew Adams, 5/30/15
 
 cp .template.tex $1.tex
-sed "s/template/$1/" <Makefile >Makefile
+sed "s/template/$1/" <Makefile >joy
+mv joy Makefile
 rm -rf .git
 git init
 git add $1.tex .template.tex .gitignore Makefile get_bibLibrary.sh initialize.sh
