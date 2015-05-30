d21a9dec838759e6a712bdc29759728bea370b4b
adamsaj <adamsaj@me.com>
2015-05-30 15:30:57 -0500

    fixes bugs in initialize script

--- initialize.sh
+++ initialize.sh
@@ -3,9 +3,10 @@
 ##usage: ./initialize.sh <document_name_without_extension>
 
 cp .template.tex $1.tex
-sed "s/template/$1/g" <Makefile >Makefile
+sed "s/template/$1/" <Makefile >Makefile
 rm -rf .git
 git init
-git add $1.tex .gitignore Makefile get_bibLibrary.sh initialize.sh
+git add $1.tex .template.tex .gitignore Makefile get_bibLibrary.sh initialize.sh
 git commit -m "initial commit"
 ./get_bibLibrary.sh
+rm LISCENCE
