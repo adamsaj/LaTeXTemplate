b637628cf6bd9791f5a0000a85076bb1dc4e25f4
adamsaj <adamsaj@me.com>
2015-05-30 15:23:51 -0500

    adds script to link BibTex library stored in iCloud

---
+++ get_bibLibrary.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+##script to make a symbolic link to BibTeX library stored on iCloud
+##by: Andrew Adams, 5/30/15
+
+ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Literature/library.bib
